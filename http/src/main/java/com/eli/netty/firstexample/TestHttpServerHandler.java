00979390d49d9120423d3da5cd216e8dee491281
周亿霖 <superyilin@163.com>
2017-05-22 22:43:20 +0800

    Fix

--- http/src/main/java/com/eli/netty/firstexample/TestHttpServerHandler.java
+++ http/src/main/java/com/eli/netty/firstexample/TestHttpServerHandler.java
@@ -16,12 +16,14 @@ public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObjec
     //读取客户端的请求并发送响应
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
-        ByteBuf content = Unpooled.copiedBuffer("Hello World", CharsetUtil.UTF_8);//向客户端返回的数据
-        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
+        if(msg instanceof HttpRequest){
+            ByteBuf content = Unpooled.copiedBuffer("Hello World", CharsetUtil.UTF_8);//向客户端返回的数据
+            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
 
-        response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
-        response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
 
-        ctx.writeAndFlush(response);
+            ctx.writeAndFlush(response);
+        }
     }
 }
