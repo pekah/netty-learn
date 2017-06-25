9e036ab68f4c45672a770becad55b93344a00d46
周亿霖 <superyilin@163.com>
2017-06-25 17:36:00 +0800

    thrift

---
+++ http/src/main/java/com/eli/netty/thrift/ThriftClient.java
@@ -0,0 +1,7 @@
+package com.eli.netty.thrift;
+
+/**
+ * Created by zhouyilin on 2017/6/19.
+ */
+public class ThriftClient {
+}
---
+++ http/src/main/java/com/eli/netty/thrift/ThriftServer.java
@@ -0,0 +1,7 @@
+package com.eli.netty.thrift;
+
+/**
+ * Created by zhouyilin on 2017/6/11.
+ */
+public class ThriftServer {
+}
