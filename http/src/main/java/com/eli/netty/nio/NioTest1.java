15d5ab2dd7ac00f6b0ec107e817b82c28e2611fc
周亿霖 <superyilin@163.com>
2017-07-22 22:00:45 +0800

    nio

---
+++ http/src/main/java/com/eli/netty/nio/NioTest1.java
@@ -0,0 +1,23 @@
+package com.eli.netty.nio;
+
+import java.nio.IntBuffer;
+
+/**
+ * Created by zhouyilin on 2017/7/22.
+ */
+public class NioTest1 {
+    public static void main(String[] args) {
+        IntBuffer intBuffer = IntBuffer.allocate(10);
+        System.out.println("capacity" + intBuffer.capacity());
+
+        for (int i = 0; i < intBuffer.capacity(); i++){
+            intBuffer.put(i);
+        }
+
+        intBuffer.flip();
+
+        while (intBuffer.hasRemaining()){
+            System.out.println(intBuffer.get());
+        }
+    }
+}
